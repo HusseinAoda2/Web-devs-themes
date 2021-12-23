eceab8798b2469bf4a4508efe1efae86486b9b27
HusseinAoda2 <husseinaoda@tutanota.com>
2021-12-24 01:30:44 +0200

    search

--- 404.php
+++ 404.php
@@ -17,6 +17,18 @@ get_header();
                                         <p>The resource requested could not be found on this server!</p>	
                                     </div>
                                 </div>
+                                <div class="col-4 d-flex justify-content-end align-items-center">
+                                    <form action="<?php /* توجيه البحث للصفحة الرئيسية */ echo home_url('/') ?>" method="get">
+                                        <input class="form-control form-control-sm" name="s">
+                                        <button type="submit" class="btn btn-sm btn-outline-dark">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
+                                                <title>Search</title>
+                                                <circle cx="10.5" cy="10.5" r="7.5" />
+                                                <path d="M21 21l-5.2-5.2" />
+                                            </svg>
+                                        </button>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
